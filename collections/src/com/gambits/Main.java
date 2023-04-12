a1b4c2b4af4bfe75479e65a42116457909ba45b0
dev-gambits <gambits-dev@gmail.com>
2023-04-12 18:14:41 -0400

    First commit

---
+++ collections/src/com/gambits/Main.java
@@ -0,0 +1,8 @@
+package com.gambits;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
---
+++ collections/src/com/gambits/fundamentals/MainCollectiosn.java
@@ -0,0 +1,9 @@
+package com.gambits.fundamentals;
+
+public class MainCollectiosn {
+    public static void main(String[] args) {
+     Integer collections = 11;
+
+
+    }
+}
